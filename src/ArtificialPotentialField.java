c6f0af596469bce8d1a55dfd3dc20cde82267cde
miaojiaxin1995 <miaojiaxin1995@163.com>
2019-12-16 17:40:14 +0800

    first commit

---
+++ src/ArtificialPotentialField.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * @program: Artificial-Potential-Field
+ * @description: artificial potential field algorithm
+ * @author: Miao Jiaxin
+ * @create: 2019-12-16 16:46
+ **/
+
+public class ArtificialPotentialField {
+    Map mMap;
+
+    public ArtificialPotentialField(Map mMap) {
+        this.mMap = mMap;
+    }
+
+    public List<Node> findPath() {
+        //TODO: main steps
+        return null;
+    }
+}
